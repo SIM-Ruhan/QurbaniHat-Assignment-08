d2c4777d1d964bf3e96352a04947649d6c09a025
SIM Ruhan <simruhan22@gmail.com>
2026-05-04 16:57:13 +0600

    NEXT_PUBLIC_BASE_URL updated

--- src/app/all-animals/page.jsx
+++ src/app/all-animals/page.jsx
@@ -10,7 +10,7 @@ const Allanimals = () => {
   const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
-    fetch("https://qurbani-hat-assignment-08-vz7v-jlxietb4y.vercel.app/data.json")
+    fetch("/data.json")
       .then((res) => res.json())
       .then((data) => {
         setModels(Array.isArray(data[0]) ? data[0] : data);
